f1ed27eec6a4a8d11f4d6d94255a235452c4d42f
Egor <egurt96@yandex.ru>
2019-11-30 23:35:01 +0300

    Create version.Ready to upload

---
+++ dist/js/script.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        // adaptiveHeight: true,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i){
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__side').eq(i).toggleClass('catalog-item__side_active');
+            })
+        })
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+//modal
+
+    $('[data-modal=consultation]').on('click',function(){
+    $('.overlay,#consultation').fadeIn(); 
+    });
+
+    $('.modal__close').on('click',function(){
+        $('.overlay,#consultation,#thanks,#order').fadeOut();
+    });
+
+    $('.button_mini').each(function(i){
+        $(this).on('click',function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+            $('.overlay,#order').fadeIn();
+        })
+    });
+
+    function validateForm(form){
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста укажите свое имя",
+                phone: "Пожалуйста укажите свой номер телефона",
+                email: {
+                  required: "Нам необходима ваша почта чтобы связаться с вами",
+                  email: "Ваша почта должна быть в формате name@domain.com",
+                }
+            }
+        });
+    };
+
+    validateForm('#consultation__form')
+    validateForm('#consultation form')
+    validateForm('#order form')
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // smooth scroll and page up
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn()
+        } else {
+            $('.pageup').fadeOut()
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html,body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+    // wow js 
+    new WOW().init();
+}); 
